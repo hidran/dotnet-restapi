1fd44e7360af28c59aed996b8a73d1e2e62dfa45
hidranarias <discoduro@hotmail.it>
2023-12-10 18:39:03 +0100

    Adds task details

--- Controllers/TasksController.cs
+++ Controllers/TasksController.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using PmsApi.DataContexts;
 using PmsApi.DTO;
+using PmsApi.Utilities;
 
 namespace PmsApi.Controllers;
 
@@ -20,22 +21,19 @@ public class TasksController : ControllerBase
     [HttpGet]
     public async Task<ActionResult<IEnumerable<TaskAllDto>>> GetTasks([FromQuery] string include = "")
     {
-        var tasksQuery = _context.Tasks.AsQueryable();
-
-        if (include.Contains("user", StringComparison.OrdinalIgnoreCase))
-        {
-            tasksQuery = tasksQuery.Include(t => t.AssignedUser);
-        }
-        if (include.Contains("project", StringComparison.OrdinalIgnoreCase))
-        {
-            tasksQuery = tasksQuery.Include(t => t.Project);
-        }
-        if (include.Contains("attachments", StringComparison.OrdinalIgnoreCase))
-        {
-            tasksQuery = tasksQuery.Include(t => t.TaskAttachments);
-        }
+        var tasksQuery = QueryHelper.ApplyIncludes(_context.Tasks.AsQueryable(), include);
         var tasks = await tasksQuery.ToListAsync();
         var tasksDto = _mapper.Map<IEnumerable<TaskAllDto>>(tasks);
+        return Ok(tasksDto);
+    }
+    [HttpGet("{taskId}")]
+    public async Task<ActionResult<TaskAllDto>>
+     GetTask(int taskId, [FromQuery] string include = "")
+    {
+        var tasksQuery = QueryHelper.ApplyIncludes(_context.Tasks.AsQueryable(), include);
+
+        var task = await tasksQuery.FirstOrDefaultAsync(x => x.TaskId == taskId);
+        var tasksDto = _mapper.Map<TaskAllDto>(task);
         return Ok(tasksDto);
     }
 
